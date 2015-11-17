caa5ac5bb396a5b284af0d578cc6ff35f65f6083
wangli <wangli1@jd.com>
2015-11-17 20:38:35 +0800

    v0.2，增加frame解析 Tips：需要将frame按照timestamp排序，最后最大

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -21,20 +21,26 @@ angular.module('myAngularApp')
     //测试数据
     $scope.layers = [
       {'layerId':1,'layerName':'图层0','layerType':'0','frames':[
-        {'frameId':0,'layerId':1,'timestamp':10,'frameType':'0','frameContent':'text1','pos-left':0,'pos-top':100,'symbols':[
+        {'frameId':0,'layerId':1,'timestamp':10,'symbolType':'0','type':'0','frameContent':'text1','pos-left':0,'pos-top':100,'symbols':[
           {'symbolId':0,'symbolContent':'text1','pos-left':0,'pos-top':0,'scale-x':1,'scale-y':1,'scale-z':1,'skew-x':1,'skew-y':1,'rotate-x':0,'rotate-y':0,'opacity':1,'animate-origin':'50% 50%'}
         ]},
-        {'frameId':1,'layerId':1,'timestamp':30,'frameType':'1','frameContent':'https://www.baidu.com/img/bd_logo1.png','pos-left':50,'pos-top':200,'symbols':[
+        {'frameId':1,'layerId':1,'timestamp':30,'symbolType':'1','type':'1','frameContent':'https://www.baidu.com/img/bd_logo1.png','pos-left':50,'pos-top':200,'symbols':[
+          {'symbolId':1,'symbolContent':'https://www.baidu.com/img/bd_logo1.png','pos-left':0,'pos-top':0,'scale-x':1,'scale-y':1,'scale-z':1,'skew-x':1,'skew-y':1,'rotate-x':0,'rotate-y':0,'opacity':1,'animate-origin':'50% 50%'}
+        ]},
+        {'frameId':1,'layerId':1,'timestamp':80,'symbolType':'1','type':'0','frameContent':'https://www.baidu.com/img/bd_logo1.png','pos-left':50,'pos-top':200,'symbols':[
           {'symbolId':1,'symbolContent':'https://www.baidu.com/img/bd_logo1.png','pos-left':0,'pos-top':0,'scale-x':1,'scale-y':1,'scale-z':1,'skew-x':1,'skew-y':1,'rotate-x':0,'rotate-y':0,'opacity':1,'animate-origin':'50% 50%'}
         ]}
       ]},
       {'layerId':2,'layerName':'图层1','layerType':'1','frames':[
-        {'frameId':3,'layerId':2,'timestamp':20,'frameType':'3','frameContent':'frameMc','pos-left':0,'pos-top':0,'symbols':[
+        {'frameId':3,'layerId':2,'timestamp':20,'symbolType':'2','type':'1','frameContent':'frameMc','pos-left':0,'pos-top':0,'symbols':[
           {'symbolId':2,'symbolContent':'text1','pos-left':0,'pos-top':0,'scale-x':1,'scale-y':1,'scale-z':1,'skew-x':1,'skew-y':1,'rotate-x':0,'rotate-y':0,'opacity':1,'animate-origin':'50% 50%'},
           {'symbolId':3,'symbolContent':'https://www.baidu.com/img/bd_logo1.png','pos-left':0,'pos-top':0,'scale-x':1,'scale-y':1,'scale-z':1,'skew-x':1,'skew-y':1,'rotate-x':0,'rotate-y':0,'opacity':1,'animate-origin':'50% 50%'}
         ]}
       ]},
-      {'layerId':3,'layerName':'图层2','layerType':'2','frames':[]}
+      {'layerId':3,'layerName':'图层2','layerType':'2','frames':[
+        {'frameId':3,'layerId':2,'timestamp':20,'symbolType':'2','type':'2','frameContent':'frameMc','pos-left':0,'pos-top':0,'symbols':[
+        ]}
+      ]}
     ];
     $scope.frames = [];
     $scope.symbols = [];
@@ -46,60 +52,62 @@ angular.module('myAngularApp')
       this.newLayer();
       this.newFrame();
       this.setScreenHeight();
+      this.anaSymbol();
     }
+    //解析帧对象
+    $scope.anaFrame = function(){
+      for(var i = 0;i<$scope.layers.length;i++){
+        $scope.frames.push($scope.layers[i].frames)
+        if($scope.layers[i].frames != []){
+          for(var j = 0;j< $scope.layers[i].frames.length;j++){
+            //$scope.frames.push($scope.layers[i].frames[j]);//frames保存为数组形式，下标为layerid对应
+            switch ($scope.layers[i].frames[j].type){
+              case '0':
+                //普通帧"frame-style"
+                    break;
+              case '1':
+                //关键帧"frame-key"
+                $('.frame-contrl').eq(i).find('span').eq($scope.layers[i].frames[j].timestamp).addClass('frame-key')
+                    break;
+              case '2':
+                //空白关键帧"frame-empty"
+                $('.frame-contrl').eq(i).find('span').eq($scope.layers[i].frames[j].timestamp).addClass('frame-empty')
+                    break;
+            }
+            if(j == $scope.layers[i].frames.length -1){
+              for(var _n = 0;_n<$scope.layers[i].frames[j].timestamp;_n++){
+                $('.frame-contrl').eq(i).find('span').eq(_n).addClass('frame-style')
+              }
+            }
+          }
+        }
+      }
+    }
+    //解析元件对象
+    $scope.anaSymbol = function(){
+      for(var i = 0;i<$scope.layers.length;i++){
+        for(var k = 0;k<$scope.layers[i].frames.length;k++){
+          if($scope.layers[i].frames[k].symbols != []){
+            for(var j = 0;j< $scope.layers[i].frames[k].symbols.length;j++){
+              $scope.symbols.push($scope.layers[i].frames[k].symbols[j])
+            }
+          }
+        }
+
+      }
+    }
+    //选中图层
+    $scope.chooseLayer = function(_index){
+      console.log('_index',_index)
+      $('.layer-contrl').removeClass('layer-choice');
+      $('.layer-contrl').eq(_index).addClass('layer-choice');
+    }
+    //新建图层
     $scope.newLayer = function () {
 
     }
+    //新建帧
     $scope.newFrame = function () {
-
-    }
-    $scope.insetFrame = function(){
-      if(currentFrame.length>0){
-
-        for(var i=0;i<currentFrame.length;i++){
-          //帧
-          for(var j=0;j<frameArray.length;j++){
-            if(currentFrame[i].split('_')[0]==frameArray[j].split('_')[0]){
-              if(parseInt(currentFrame[i].split('_')[1])>parseInt(frameArray[j].split('_')[1])){
-                frameArray[j]=currentFrame[i];
-              }
-
-              else{
-                frameArray[j]=frameArray[j].split('_')[0]+'_'+(parseInt(frameArray[j].split('_')[1])+1);
-              }
-            }
-          }
-          //关键帧
-          for(var j=keyFrameArray.length-1;j>=0;j--){
-            if(currentFrame[i].split('_')[0]==keyFrameArray[j].split('_')[0]){
-              if(parseInt(currentFrame[i].split('_')[1])<parseInt(keyFrameArray[j].split('_')[1])){
-                keyFrameArray[j]=keyFrameArray[j].split('_')[0]+'_'+(parseInt(keyFrameArray[j].split('_')[1])+1);
-              }
-            }
-          }
-          //空白关键帧
-          for(var j=emptyKeyFrameArray.length-1;j>=0;j--){
-            if(currentFrame[i].split('_')[0]==emptyKeyFrameArray[j].split('_')[0]){
-              if(parseInt(currentFrame[i].split('_')[1])<parseInt(emptyKeyFrameArray[j].split('_')[1])){
-                emptyKeyFrameArray[j]=emptyKeyFrameArray[j].split('_')[0]+'_'+(parseInt(emptyKeyFrameArray[j].split('_')[1])+1);
-              }
-            }
-          }
-          //关键帧
-          for(var j=actionArray.length-1;j>=0;j--){
-            if(currentFrame[i].split('_')[0]==actionArray[j].split('_')[0]){
-              if(parseInt(currentFrame[i].split('_')[1])<parseInt(actionArray[j].split('_')[1])){
-                actionArray[j]=actionArray[j].split('_')[0]+'_'+(parseInt(actionArray[j].split('_')[1])+1);
-              }
-            }
-          }
-
-        }
-        keyFrameShow();
-        emptyKeyFrameShow();
-        lastFrameShow();
-        actionShow();
-      }
 
     }
     $scope.insetKeyFrame = function () {
@@ -120,6 +128,10 @@ angular.module('myAngularApp')
         $('.timeline-numb-show').css('width',$scope.screenOpt.width-210);
     }
     $scope.init();
+    $scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
+      $scope.chooseLayer(0);//默认选中第一个图层
+      $scope.anaFrame();;//将frame状态显示出来
+    });
     //时间轴滑动@fixme
     //比例
     var framePercent;
@@ -168,44 +180,21 @@ angular.module('myAngularApp')
       }
     });
     //绑定键盘快捷键 End
-    //update frame
-    $scope.updateFrame = function(_frame){
-      $scope.frames = _frame;
-    }
+
   })
   .controller('layerCtrl',function($scope){
 
-      $scope.init = function(){
 
-      }
-    $scope.init();
   })
   .controller('frameCtrl',function($scope){
-    $scope.frames = [];
-    for(var i = 0;i<$scope.layers.length;i++){
-      if($scope.layers[i].frames != []){
-        for(var j = 0;j< $scope.layers[i].frames.length;j++){
-          $scope.frames.push($scope.layers[i].frames[j])
-        }
-      }
-    }
-    $scope.updateFrame($scope.frames);
+
+
   })
   .controller('toolCtrl',function($scope){
 
   })
   .controller('symbolCtrl',function($scope){
-    $scope.symbols = [];
-    for(var i = 0;i<$scope.layers.length;i++){
-      for(var k = 0;k<$scope.layers[i].frames.length;k++){
-        if($scope.layers[i].frames[k].symbols != []){
-          for(var j = 0;j< $scope.layers[i].frames[k].symbols.length;j++){
-            $scope.symbols.push($scope.layers[i].frames[k].symbols[j])
-          }
-        }
-      }
 
-    }
 
   }).controller('editCtrl',function($scope){
 
@@ -219,4 +208,15 @@ angular.module('myAngularApp')
 
     return input;
   };
-});
+}).directive('onFinishRenderFilters', function ($timeout) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attr) {
+        if (scope.$last === true) {
+          $timeout(function() {
+            scope.$emit('ngRepeatFinished');
+          });
+        }
+      }
+    };
+  });;
