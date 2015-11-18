0d208c403c180877ff6102d8c040f388d301cecc
wangli <wangli1@jd.com>
2015-11-18 21:08:19 +0800

    v0.3，增加元件解析、时间轴显示当前画布 Tips：需要将frame按照timestamp排序，最后最大

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -20,30 +20,32 @@ angular.module('myAngularApp')
     }
     //测试数据
     $scope.layers = [
-      {'layerId':1,'layerName':'图层0','layerType':'0','frames':[
-        {'frameId':0,'layerId':1,'timestamp':10,'symbolType':'0','type':'0','frameContent':'text1','pos-left':0,'pos-top':100,'symbols':[
-          {'symbolId':0,'symbolContent':'text1','pos-left':0,'pos-top':0,'scale-x':1,'scale-y':1,'scale-z':1,'skew-x':1,'skew-y':1,'rotate-x':0,'rotate-y':0,'opacity':1,'animate-origin':'50% 50%'}
+      {'layerId':0,'layerName':'图层0','layerType':'0','frames':[
+        {'frameId':0,'layerId':1,'timestamp':10,'symbolType':'0','type':'0','frameContent':'text1','posLeft':0,'posTop':100,'symbols':[
+          {'symbolId':0,'frameId':0,'symbolType':0,'symbolContent':'text1','posLeft':10,'posTop':0,'scaleX':1,'scaleY':1,'scaleZ':1,'skewX':1,'skewY':1,'rotateX':0,'rotateY':0,'opacity':1,'origin':'50% 50%'}
         ]},
-        {'frameId':1,'layerId':1,'timestamp':30,'symbolType':'1','type':'1','frameContent':'https://www.baidu.com/img/bd_logo1.png','pos-left':50,'pos-top':200,'symbols':[
-          {'symbolId':1,'symbolContent':'https://www.baidu.com/img/bd_logo1.png','pos-left':0,'pos-top':0,'scale-x':1,'scale-y':1,'scale-z':1,'skew-x':1,'skew-y':1,'rotate-x':0,'rotate-y':0,'opacity':1,'animate-origin':'50% 50%'}
+        {'frameId':1,'layerId':1,'timestamp':30,'symbolType':'1','type':'1','frameContent':'https://www.baidu.com/img/bd_logo1.png','posLeft':50,'posTop':200,'symbols':[
+          {'symbolId':1,'frameId':1,'symbolType':1,'symbolContent':'https://www.baidu.com/img/bd_logo1.png','posLeft':0,'posTop':0,'scaleX':1,'scaleY':1,'scaleZ':1,'skewX':1,'skewY':1,'rotateX':0,'rotateY':0,'opacity':1,'origin':'50% 50%'}
         ]},
-        {'frameId':1,'layerId':1,'timestamp':80,'symbolType':'1','type':'0','frameContent':'https://www.baidu.com/img/bd_logo1.png','pos-left':50,'pos-top':200,'symbols':[
-          {'symbolId':1,'symbolContent':'https://www.baidu.com/img/bd_logo1.png','pos-left':0,'pos-top':0,'scale-x':1,'scale-y':1,'scale-z':1,'skew-x':1,'skew-y':1,'rotate-x':0,'rotate-y':0,'opacity':1,'animate-origin':'50% 50%'}
+        {'frameId':2,'layerId':1,'timestamp':80,'symbolType':'1','type':'0','frameContent':'https://www.baidu.com/img/bd_logo1.png','posLeft':50,'posTop':200,'symbols':[
+          {'symbolId':2,'frameId':2,'symbolType':1,'symbolContent':'https://www.baidu.com/img/bd_logo1.png','posLeft':0,'posTop':0,'scaleX':1,'scaleY':1,'scaleZ':1,'skewX':1,'skewY':1,'rotateX':0,'rotateY':0,'opacity':1,'origin':'50% 50%'}
         ]}
       ]},
-      {'layerId':2,'layerName':'图层1','layerType':'1','frames':[
-        {'frameId':3,'layerId':2,'timestamp':20,'symbolType':'2','type':'1','frameContent':'frameMc','pos-left':0,'pos-top':0,'symbols':[
-          {'symbolId':2,'symbolContent':'text1','pos-left':0,'pos-top':0,'scale-x':1,'scale-y':1,'scale-z':1,'skew-x':1,'skew-y':1,'rotate-x':0,'rotate-y':0,'opacity':1,'animate-origin':'50% 50%'},
-          {'symbolId':3,'symbolContent':'https://www.baidu.com/img/bd_logo1.png','pos-left':0,'pos-top':0,'scale-x':1,'scale-y':1,'scale-z':1,'skew-x':1,'skew-y':1,'rotate-x':0,'rotate-y':0,'opacity':1,'animate-origin':'50% 50%'}
+      {'layerId':1,'layerName':'图层1','layerType':'1','frames':[
+        {'frameId':3,'layerId':2,'timestamp':20,'symbolType':'2','type':'1','frameContent':'frameMc','posLeft':0,'posTop':0,'symbols':[
+          {'symbolId':3,'frameId':3,'symbolType':0,'symbolContent':'text1','posLeft':0,'posTop':0,'scaleX':1,'scaleY':1,'scaleZ':1,'skewX':1,'skewY':1,'rotateX':0,'rotateY':0,'opacity':1,'origin':'50% 50%'},
+          {'symbolId':4,'frameId':3,'symbolType':1,'symbolContent':'https://www.baidu.com/img/bd_logo1.png','posLeft':0,'posTop':0,'scaleX':1,'scaleY':1,'scaleZ':1,'skewX':1,'skewY':1,'rotateX':0,'rotateY':0,'opacity':1,'origin':'50% 50%'}
         ]}
       ]},
-      {'layerId':3,'layerName':'图层2','layerType':'2','frames':[
-        {'frameId':3,'layerId':2,'timestamp':20,'symbolType':'2','type':'2','frameContent':'frameMc','pos-left':0,'pos-top':0,'symbols':[
+      {'layerId':2,'layerName':'图层2','layerType':'2','frames':[
+        {'frameId':4,'layerId':3,'timestamp':20,'symbolType':'2','type':'2','frameContent':'frameMc','posLeft':0,'posTop':0,'symbols':[
         ]}
       ]}
     ];
     $scope.frames = [];
     $scope.symbols = [];
+    $scope.curTime = 0;
+    $scope.curFrameIndex = 0;
     //初始化
     $scope.init = function () {
       this.canShowFrame = false;
@@ -67,11 +69,11 @@ angular.module('myAngularApp')
                     break;
               case '1':
                 //关键帧"frame-key"
-                $('.frame-contrl').eq(i).find('span').eq($scope.layers[i].frames[j].timestamp).addClass('frame-key')
+                $('.frame-contrl').eq(i).find('span').eq($scope.layers[i].frames[j].timestamp-1).addClass('frame-key')
                     break;
               case '2':
                 //空白关键帧"frame-empty"
-                $('.frame-contrl').eq(i).find('span').eq($scope.layers[i].frames[j].timestamp).addClass('frame-empty')
+                $('.frame-contrl').eq(i).find('span').eq($scope.layers[i].frames[j].timestamp-1).addClass('frame-empty')
                     break;
             }
             if(j == $scope.layers[i].frames.length -1){
@@ -86,15 +88,18 @@ angular.module('myAngularApp')
     //解析元件对象
     $scope.anaSymbol = function(){
       for(var i = 0;i<$scope.layers.length;i++){
+        var tmpSymbol = [];
         for(var k = 0;k<$scope.layers[i].frames.length;k++){
+          //tmpSymbol = [];
           if($scope.layers[i].frames[k].symbols != []){
             for(var j = 0;j< $scope.layers[i].frames[k].symbols.length;j++){
-              $scope.symbols.push($scope.layers[i].frames[k].symbols[j])
+              tmpSymbol.push($scope.layers[i].frames[k].symbols[j])
             }
           }
         }
-
+        $scope.symbols.push(tmpSymbol)
       }
+      console.log('1111',$scope.symbols)
     }
     //选中图层
     $scope.chooseLayer = function(_index){
@@ -116,6 +121,34 @@ angular.module('myAngularApp')
     $scope.insetEmptyKeyFrame = function () {
 
     }
+    //显示当前画布状态
+    $scope.showCur = function(_layer,_timestamp){
+      $scope.curTime =_timestamp;
+      //图层状态
+      $('.layer-contrl').removeClass('layer-choice');
+      $('.layer-contrl').eq(_layer).addClass('layer-choice');
+      //帧状态
+      $('.frame-contrl span').removeClass('current');
+      $('.frame-contrl').eq(_layer).find('span').eq(_timestamp-1).addClass('current');
+      //元件状态
+      $('.stage-wrap').removeClass('current');
+      $('.stage-wrap').eq(_layer).addClass('current');
+      for(var i = $scope.frames[_layer].length-1;i>=0;i--){
+        if($scope.frames[_layer][0].timestamp > _timestamp){
+          $scope.curFrameIndex = -1;
+          break;
+        }
+        if($scope.frames[_layer][i].timestamp <= _timestamp){
+          $scope.curFrameIndex = $scope.frames[_layer][i].frameId;
+          break;
+        }
+      }
+      var attrFrameId = $scope.curFrameIndex;
+      $('.stage-wrap.current .symbolWrap').removeClass('current');
+      if(attrFrameId != -1){
+        $('.stage-wrap.current .symbolWrap[data-frame='+attrFrameId+']').addClass('current');
+      }
+    }
     $scope.setScreenHeight = function(){
       //屏幕高宽----------------------------------------------------------------------------
       $scope.screenOpt.height= $(window).height()-$('.top-bar').height();
@@ -129,8 +162,9 @@ angular.module('myAngularApp')
     }
     $scope.init();
     $scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
-      $scope.chooseLayer(0);//默认选中第一个图层
-      $scope.anaFrame();;//将frame状态显示出来
+      //$scope.chooseLayer(0);//默认选中第一个图层
+      $scope.anaFrame();//将frame状态显示出来
+      $scope.showCur(0,0);//显示当前的画布状态：图层、时间确定当前元件
     });
     //时间轴滑动@fixme
     //比例
